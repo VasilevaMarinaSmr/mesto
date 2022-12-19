913c7b17cc44f93da0d80cd8907e8f7a61b4b2f8
Marina Vasileva <mv031a@yandex.ru>
2022-12-19 14:07:10 +0400

    feat: создать кнопку закрыть на 240

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 const popup = document.querySelector('.popup');
+let inputPopupName = document.querySelector("#Popup_form_name");
+let inputPopupProfession = document.querySelector("#Popup_form_profession");
+let nameProfile = document.querySelector(".profile-info__name");
+let professionProfile = document.querySelector(".profile-info__profession");
+inputPopupName.value = nameProfile.textContent
+inputPopupProfession.value = professionProfile.textContent
 popup.classList.add('popup_opened');
