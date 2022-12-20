ce5502e03bf9bca1b9b047967e020899494ed2c2
Marina Vasileva <mv031a@yandex.ru>
2022-12-20 16:56:48 +0300

    feat: Анимировать popup

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,30 @@
-const popup = document.querySelector('.popup');
-let inputPopupName = document.querySelector("#Popup_form_name");
-let inputPopupProfession = document.querySelector("#Popup_form_profession");
-let nameProfile = document.querySelector(".profile-info__name");
-let professionProfile = document.querySelector(".profile-info__profession");
-inputPopupName.value = nameProfile.textContent
-inputPopupProfession.value = professionProfile.textContent
-// popup.classList.add('popup_opened');
+const popupForm = document.querySelector('.popup');
+let popupInputName = document.querySelector("#Popup_form_name");
+let popupInputProfession = document.querySelector("#Popup_form_profession");
+let profileName = document.querySelector(".profile-info__name");
+let profileProfession = document.querySelector(".profile-info__profession");
+let profileOpenPopupBtn = document.querySelector('profile-info__edit-button');
+let popupCloseBtn = popupForm.querySelector('.popup__button-close');
+
+
+
+function openPopup() {
+  popupForm.classList.add('popup_opened');
+  popupInputName.value = profileName.textContent
+  popupInputProfession.value = profileProfession.textContent
+}
+
+function closePopup() {
+  popupForm.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputName.value;
+  profileProfession.textContent = popupInputProfession.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+profileOpenPopupBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
